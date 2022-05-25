6f7cb9e7c28908f6d655661fe4c1b521e2ad298a
jim_kim_ <75765858+jim-kim-playground@users.noreply.github.com>
2022-05-25 23:08:09 +0900

    Updated main func to use $PORT

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+	
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	} else {
+		log.Println("$PORT is " + port)
+	}
+
 	r := gin.Default()
+	r.Use(gin.Logger())
+	
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run(":3000")
+
+	r.Run(":" + port)
 }
